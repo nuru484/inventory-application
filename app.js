fab57361bf39c98e86cbeaed04ef792d7259be36
Nurudeen <abdulmajeednurudeen48@gmail.com>
2024-09-09 04:17:26 +1200

    Removed unnecessary files

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ PORT = process.env.PORT || 3000;
 const app = express();
 
 app.set('view engine', 'ejs');
-app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
